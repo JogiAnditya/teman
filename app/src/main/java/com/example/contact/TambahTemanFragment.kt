4041508e9f381606e80093b26ec9ce9b1febd60e
JogiAnditya <jogidhimas51@gmail.com>
2019-05-11 13:40:28 +0700

    first commit

---
+++ app/src/main/java/com/example/contact/TambahTemanFragment.kt
@@ -0,0 +1,29 @@
+package com.example.contact
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
+
+class TambahTemanFragment: Fragment () {
+
+    companion object {
+        fun newInstance(): TambahTemanFragment {
+            return TambahTemanFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.addfriend, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+}
